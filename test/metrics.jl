2a68f2a1571c9bf567027f725a0dc6ffad27a8fe
Arthur Lui <luiarthur@gmail.com>
2020-12-08 20:36:35 -0700

    shortened tests

--- test/metrics.jl
+++ test/metrics.jl
@@ -2,7 +2,6 @@ println("Test metrics")
 
 import Distributions.SpecialFunctions.logabsbeta
 import Distributions.StatsBase.harmmean
-
 lbeta(a, b) = logabsbeta(a, b)[1]
 
 @testset "metrics" begin
@@ -33,12 +32,15 @@ lbeta(a, b) = logabsbeta(a, b)[1]
 
     true_log_bf = c1_true - c0_true
 
-    post_samps_m0 = rand(Beta(m0.α + sumy, m0.β + n - sumy), 1_000_000)
-    post_samps_m1 = rand(Beta(m1.α + sumy, m1.β + n - sumy), 1_000_000)
+    post_samps_m0 = rand(Beta(m0.α + sumy, m0.β + n - sumy), 100_000)
+    post_samps_m1 = rand(Beta(m1.α + sumy, m1.β + n - sumy), 100_000)
     ll0 = map(p -> sum(logpdf.(Bernoulli(p), y)), post_samps_m0)
     ll1 = map(p -> sum(logpdf.(Bernoulli(p), y)), post_samps_m1)
 
     approx_log_bf = log_bayes_factor(ll0, ll1)
+
+    @test c0_true ≈ MCMC.logharmmeanexp(ll0)  atol=0.2
+    @test c1_true ≈ MCMC.logharmmeanexp(ll1)  atol=0.2
 
     @test true_log_bf ≈ approx_log_bf atol=0.2
   end
